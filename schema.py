bf58d10c7873b678e80be8dbc3978e75bc849c19
J0hnBloodborne <m.rohaan.mirza@gmail.com>
2025-12-14 10:58:12 +0500

    steps 8 and 9 done

--- schema.py
+++ schema.py
@@ -3,19 +3,21 @@ from rdflib.collection import Collection
 
 # 1. Setup
 ER = Namespace("http://www.semanticweb.org/fall2025/eldenring/")
-WD = Namespace("http://www.wikidata.org/entity/") # External Link
 g = Graph()
 g.bind("er", ER)
 g.bind("owl", OWL)
-g.bind("wd", WD)
 
 ontology_uri = URIRef("http://www.semanticweb.org/fall2025/eldenring/")
 g.add((ontology_uri, RDF.type, OWL.Ontology))
 
-# --- CLASSES ---
+# ==========================================
+# CLASSES
+# ==========================================
 classes = [
-    "Agent", "Boss", "NPC", "Creature", "Shardbearer",
-    "Location", "Item", "Weapon", "Armor", "Talisman", "Remembrance",
+    "Agent", "Boss", "NPC", "Creature", "Shardbearer", "Demigod", "MinorEnemy",
+    "Location", "SiteOfGrace",
+    "Item", "Weapon", "Shield", "Armor", "Talisman", "Consumable", "Material", "KeyItem", "Remembrance", "GreatRune",
+    "Magic", "Sorcery", "Incantation",
     "Affinity", "Attribute", "Cookbook", "Whetblade"
 ]
 for c in classes:
@@ -26,72 +28,80 @@ g.add((ER.Boss, RDFS.subClassOf, ER.Agent))
 g.add((ER.NPC, RDFS.subClassOf, ER.Agent))
 g.add((ER.Creature, RDFS.subClassOf, ER.Agent))
 g.add((ER.Weapon, RDFS.subClassOf, ER.Item))
+g.add((ER.Shield, RDFS.subClassOf, ER.Item))
+g.add((ER.Armor, RDFS.subClassOf, ER.Item))
+g.add((ER.Talisman, RDFS.subClassOf, ER.Item))
+g.add((ER.Consumable, RDFS.subClassOf, ER.Item))
+g.add((ER.Material, RDFS.subClassOf, ER.Item))
+g.add((ER.KeyItem, RDFS.subClassOf, ER.Item))
 g.add((ER.Remembrance, RDFS.subClassOf, ER.Item))
+g.add((ER.GreatRune, RDFS.subClassOf, ER.Item))
+g.add((ER.Cookbook, RDFS.subClassOf, ER.Item))
+g.add((ER.Whetblade, RDFS.subClassOf, ER.Item))
+g.add((ER.Sorcery, RDFS.subClassOf, ER.Magic))
+g.add((ER.Incantation, RDFS.subClassOf, ER.Magic))
 
-# --- RUBRIC REQUIREMENT: ENUMERATION ---
-# Class 'Affinity' is defined as specific individuals only
+# ==========================================
+# LOGICAL DEFINITIONS
+# ==========================================
+
+# 1. ENUMERATION (Affinity)
 affinities = [ER.Standard, ER.Heavy, ER.Keen, ER.Quality, ER.Magic, ER.Fire]
-g.add((ER.Affinity, RDF.type, OWL.Class))
 enum_node = BNode()
 Collection(g, enum_node, affinities)
 g.add((ER.Affinity, OWL.oneOf, enum_node))
 
-# --- RUBRIC REQUIREMENT: INTERSECTION ---
-# Demigod = Boss AND Shardbearer
-demigod = ER.Demigod
-g.add((demigod, RDF.type, OWL.Class))
+# 2. UNION (Magic)
+union_node = BNode()
+Collection(g, union_node, [ER.Sorcery, ER.Incantation])
+g.add((ER.Magic, OWL.unionOf, union_node))
+
+# 3. INTERSECTION (Demigod)
 inter_node = BNode()
 Collection(g, inter_node, [ER.Boss, ER.Shardbearer])
-g.add((demigod, OWL.intersectionOf, inter_node))
+g.add((ER.Demigod, OWL.intersectionOf, inter_node))
 
-# --- RUBRIC REQUIREMENT: COMPLEMENT ---
-# MinorEnemy = Creature AND (NOT Boss)
-minor_enemy = ER.MinorEnemy
-g.add((minor_enemy, RDF.type, OWL.Class))
-# Define NOT Boss
+# 4. COMPLEMENT (MinorEnemy)
 not_boss = BNode()
 g.add((not_boss, OWL.complementOf, ER.Boss))
-# Define Intersection
-compl_inter_node = BNode()
-Collection(g, compl_inter_node, [ER.Creature, not_boss])
-g.add((minor_enemy, OWL.intersectionOf, compl_inter_node))
+compl_node = BNode()
+Collection(g, compl_node, [ER.Creature, not_boss])
+g.add((ER.MinorEnemy, OWL.intersectionOf, compl_node))
 
-# --- PROPERTIES ---
+# ==========================================
+# PROPERTIES
+# ==========================================
 
-# 1. Functional Property (Rubric)
-# An agent can only be primarily located in one place
-p_loc = ER.locatedIn
-g.add((p_loc, RDF.type, OWL.ObjectProperty))
-g.add((p_loc, RDF.type, OWL.FunctionalProperty))
-g.add((p_loc, RDFS.domain, ER.Agent))
-g.add((p_loc, RDFS.range, ER.Location))
+# FIX: locatedIn Domain = Union(Agent, SiteOfGrace)
+# This lets Sites of Grace be "locatedIn" a Location too.
+loc_domain = BNode()
+Collection(g, loc_domain, [ER.Agent, ER.SiteOfGrace])
+g.add((ER.locatedIn, RDF.type, OWL.ObjectProperty))
+g.add((ER.locatedIn, RDF.type, OWL.FunctionalProperty))
+g.add((ER.locatedIn, RDFS.domain, loc_domain)) # <--- THE FIX
+g.add((ER.locatedIn, RDFS.range, ER.Location))
 
-# 2. Inverse Functional Property (Rubric)
-# A specific Remembrance uniquely identifies the Boss who dropped it
-p_rem = ER.droppedBy
-g.add((p_rem, RDF.type, OWL.ObjectProperty))
-g.add((p_rem, RDF.type, OWL.InverseFunctionalProperty))
-g.add((p_rem, RDFS.domain, ER.Remembrance))
-g.add((p_rem, RDFS.range, ER.Boss))
-
-# 3. Standard Properties with Ranges (Rubric)
-props = {
+# Other Props
+obj_props = {
+    "droppedBy": (ER.Remembrance, ER.Boss),
     "scalesWith": (ER.Weapon, ER.Attribute),
     "unlocksAffinity": (ER.Whetblade, ER.Affinity),
-    "unlocksRecipeFor": (ER.Cookbook, ER.Item)
+    "unlocksRecipeFor": (ER.Cookbook, ER.Item),
+    "requiresStat": (ER.Weapon, ER.Attribute),
+    "hasEffect": (ER.Talisman, XSD.string)
 }
-for p_name, (dom, rng) in props.items():
-    p = ER[p_name]
-    g.add((p, RDF.type, OWL.ObjectProperty))
-    g.add((p, RDFS.domain, dom))
-    g.add((p, RDFS.range, rng))
+for p, (dom, rng) in obj_props.items():
+    g.add((ER[p], RDF.type, OWL.ObjectProperty))
+    g.add((ER[p], RDFS.domain, dom))
+    g.add((ER[p], RDFS.range, rng))
 
-# 4. Datatype Properties
-g.add((ER.gameId, RDF.type, OWL.DatatypeProperty))
-g.add((ER.gameId, RDFS.range, XSD.integer))
-g.add((ER.hasWeight, RDF.type, OWL.DatatypeProperty))
-g.add((ER.hasWeight, RDFS.range, XSD.float))
+g.add((ER.droppedBy, RDF.type, OWL.InverseFunctionalProperty))
 
-# Output
+# Datatype Props
+data_props = ["gameId", "hasWeight", "description", "image"]
+for p in data_props:
+    g.add((ER[p], RDF.type, OWL.DatatypeProperty))
+    g.add((ER[p], RDFS.range, XSD.float if p == "hasWeight" else (XSD.integer if p == "gameId" else XSD.string)))
+
 g.serialize(destination="rdf/elden_ring_schema.ttl", format='turtle')
-print("T-Box Generated.")
+print("Fixed Schema: Sites of Grace are now connected.")
